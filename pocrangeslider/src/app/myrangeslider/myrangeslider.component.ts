32414492b466cb0f9a1dba785c6f42e7da0bfcfc
SunilP <spottumuttu@osius.com>
2021-05-30 20:32:39 +0530

    changed to use promises

--- pocrangeslider/src/app/myrangeslider/myrangeslider.component.ts
+++ pocrangeslider/src/app/myrangeslider/myrangeslider.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { get } from 'scriptjs';
 declare var MyRangeSlider: Function;
 
 @Component({
@@ -10,8 +11,15 @@ export class MyrangesliderComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-    MyRangeSlider("this is a test message");
+  ngOnInit(): void{
+    
+    get("https://d3js.org/d3.v5.min.js", () => {
+      console.log("d3js loaded");
+      get("https://unpkg.com/data-driven-range-slider@1.0.1/index.js", () => {
+        console.log("data-driven-range-slider has been loaded...");
+        MyRangeSlider("this is a test message");
+      });
+    });
   }
 
 }
